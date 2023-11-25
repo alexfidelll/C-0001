a7e7658a8715971d3ec77dc147dcce20b48a42e0
fidel.alex <alekseifidel@gmail.com>
2023-11-25 21:40:21 +0500

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int firstValue = int.Parse(args[0]);
+int lastValue = int.Parse(args[2]);
+
+switch (args[1])
+{
+	case "+":
+        Console.WriteLine(firstValue + lastValue);
+        break;
+    case "-":
+        Console.WriteLine(firstValue - lastValue);
+        break;
+    case "/":
+        if (firstValue == 0 && lastValue == 0)
+        {
+            Console.WriteLine("Делить на 0 нельзя");
+            break;
+        }
+        Console.WriteLine(firstValue / lastValue);
+        break;
+    case "*":
+        Console.WriteLine(firstValue * lastValue);
+        break;
+
+}
